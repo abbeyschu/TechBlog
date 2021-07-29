18d129e3cda53fd0ad272a5b21f6d72ff5ae2356
Abbey Schueller <schuellerabbey@gmail.com>
2021-07-28 20:59:50 -0700

    updated views

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,24 +1,25 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/results');
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  document
-    .querySelector('#signup-form')
-    .addEventListener('click', signupFormHandler);
+  }
+};
+
+document
+  .querySelector('#signup-form')
+  .addEventListener('click', signupFormHandler);
