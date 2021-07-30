33def138e4a5d7efe6e0e2d8caa8a0de72820499
Abbey Schueller <schuellerabbey@gmail.com>
2021-07-29 22:21:11 -0700

    logout

--- public/js/logout.js
+++ public/js/logout.js
@@ -5,7 +5,7 @@ const logout = async () => {
     });
   
     if (response.ok) {
-      document.location.replace('/search');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
